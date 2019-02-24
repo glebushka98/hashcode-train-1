e317d9582d0b4a05bc09a7a7d5765b889dda70a0
Mikhail Gorbushin <m.gorbushin@corp.mail.ru>
2019-02-24 12:31:11 +0300

    add test for fromlatlon + fromlatlon

--- main.cpp
+++ main.cpp
@@ -3,14 +3,60 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <cmath>
 using namespace std;
+
+struct Point
+{
+  Point(double x, double y): x(x), y(y) {}
+  Point operator-(const Point & point) {
+    return {x - point.x, y - point.y};
+  }
+
+  double x, y;
+};
+
+namespace helper {
+template <typename TFloat> inline TFloat DegToRad(TFloat deg)
+{
+  return deg * TFloat(M_PI) / TFloat(180);
+}
+
+template <typename T>
+inline T clamp(T const x, T const xmin, T const xmax)
+{
+  if (x > xmax)
+    return xmax;
+  if (x < xmin)
+    return xmin;
+  return x;
+}
+template <typename TFloat> inline TFloat RadToDeg(TFloat rad)
+{
+  return rad * TFloat(180) / TFloat(M_PI);
+}
+static double constexpr kMinY = -180.0;
+static double constexpr kMaxY = 180.0;
+static double LonToX(double lon) { return lon; }
+static double ClampY(double d) { return clamp(d, kMinY, kMaxY); }
+static double LatToY(double lat)
+{
+  double const sinx = sin(DegToRad(clamp(lat, -86.0, 86.0)));
+  double const res = RadToDeg(0.5 * log((1.0 + sinx) / (1.0 - sinx)));
+  return ClampY(res);
+}
+static Point FromLatLon(double lat, double lon)
+{
+  return {LonToX(lon), LatToY(lat)};
+}
+
+}  // namespace
 
 const vector<string> files = {};
 
 #ifndef TEST
 
 int main() {
-
 
 }
 
